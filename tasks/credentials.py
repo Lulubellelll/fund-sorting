8790faea6fefc42635fd3cfb184fd8d16aea254f
Lulubellelll <uygar6390@gmail.com>
2024-05-23 12:47:06 +0300

    logger fixed

--- tasks/credentials.py
+++ tasks/credentials.py
@@ -1,4 +1,22 @@
 import os 
+import logging 
+import logging.handlers
+
+# Setting logger 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8"
+)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+
 
 try:
     PRIVATE_KEY_ID = os.environ["PRIVATE_KEY_ID"]
